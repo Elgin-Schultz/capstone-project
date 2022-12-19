856cc697828b12c41cc404c634454cbafc4e0a99
Elgin Schultz <elgin@elginschultz.com>
2022-12-19 16:06:42 +0100

    refactoring

--- pages/singlesite/index.js
+++ pages/singlesite/index.js
@@ -1,21 +1,27 @@
 import Link from "next/link";
-import Header from "../../components/header";
 import styled from "styled-components";
-import { allSites } from "../../lib/sites";
+import Header from "../../components/header";
+import SitesListComp from "../../components/siteslistComp";
 
 export default function SitesList() {
   return (
     <>
       <Header />
-      <ul>
-        {allSites.map((site) => (
-          <li key={site.slug}>
-            <Link href={`/singlesite/${site.slug}`}>
-              <mark>{site.coordinates[0] + " | " + site.coordinates[1]}</mark>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <SitesListComp />
     </>
   );
 }
+
+/*export default function SitesList() {
+  return (
+    <ul>
+      {allSites.map((site) => (
+        <li key={site.slug}>
+          <Link href={`/singlesite/${site.slug}`}>
+            <mark>{site.coordinates[0] + " | " + site.coordinates[1]}</mark>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}*/
