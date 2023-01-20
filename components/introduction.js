6fa99a88d1739c6ec94f83b1177b9e33f0f06528
Elgin Schultz <elgin@elginschultz.com>
2023-01-20 13:35:07 +0100

    edit introduction

--- components/introduction.js
+++ components/introduction.js
@@ -4,20 +4,21 @@ export default function Introduction() {
   return (
     <p className="introduction">
       <StyledSpan>
-        Auf folgenden Seiten versammelt sich eine Auswahl ehemaliger
-        Kultusstätten Hamburgs, die mittlerweile profaniert sind. Bei dieser
-        Zusammenstellung kommt es mir dabei nicht auf eine bestimmte Definition
-        von Glaube an, so finden sich hier frühere jüdische, christliche und
-        heidnische Plätze.
-        <br></br>
-        Alle Stellen gebe ich mit ihren geografischen Koordinaten an, dazu alte
-        und neue Ortsbezeichnungen. Mit meinen Fotografien möchte ich gerne die
-        Stimmung der heutigen Plätze festhalten – lässt sich hier noch etwas
-        Kultisches erahnen? Sie stellen keine Übersicht über den jetzigen
-        Zustand dar. Ihnen entgegen habe ich historisches Bildmaterial gesetzt,
-        das den zeitlichen Wandel der jeweiligen Orte zeigt. Die Dokumente habe
-        ich möglichst so herausgesucht, dass sie uns etwas über den Grund der
-        Profanierung verraten. Im Kontrast dazu stehen die ausgewählten Zitate.
+        Im Folgenden lässt sich eine Auswahl mittlerweile profanierter,
+        ehemaliger Kultusstätten Hamburgs entdecken. Teilweise ist ihre sakrale
+        Bedeutung noch zu erahnen, teilweise ist davon nichts mehr erkennbar.
+        Dieser Zusammenstellung kommt es dabei nicht auf ein bestimmtes
+        Verständnins von Religion an, so finden sich hier frühere jüdische,
+        christliche und heidnische Plätze.
+        <br />
+        Alle Stätten sind mit ihren geografischen Koordinaten angegeben, dazu
+        alte und neue Ortsbezeichnungen. Mit den Farbfotografien möchte ich
+        gerne die Stimmung der heutigen Plätze festhalten, sie sollen keine
+        Übersicht über den jetzigen Zustand darstellen. Ihnen entgegen steht das
+        historische Bildmaterial, das den zeitlichen Wandel der jeweiligen Orte
+        zeigt. Die Dokumente sind möglichst so herausgesucht, dass sie uns etwas
+        über den Grund der Profanierung verraten. Im Kontrast dazu stehen die
+        ausgewählten Zitate.
       </StyledSpan>
     </p>
   );
