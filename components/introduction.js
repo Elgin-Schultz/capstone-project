13dc4550418d5e44cbc64253f09cade4d03fa6c2
Elgin Schultz <elgin@elginschultz.com>
2023-01-22 15:56:16 +0100

    edit introduction

--- components/introduction.js
+++ components/introduction.js
@@ -6,19 +6,19 @@ export default function Introduction() {
       <StyledSpan>
         Im Folgenden lässt sich eine Auswahl mittlerweile profanierter,
         ehemaliger Kultusstätten Hamburgs entdecken. Teilweise ist ihre sakrale
-        Bedeutung noch zu erahnen, teilweise ist davon nichts mehr erkennbar.
-        Dieser Zusammenstellung kommt es dabei nicht auf ein bestimmtes
-        Verständnins von Religion an, so finden sich hier frühere jüdische,
-        christliche und heidnische Plätze.
+        Bedeutung noch zu erahnen, teils ist davon nichts mehr erkennbar. Dieser
+        Zusammenstellung kommt es dabei nicht auf ein bestimmtes Verständnins
+        von Glauben an, so finden sich hier frühere jüdische, christliche und
+        heidnische Plätze.
         <br />
         Alle Stätten sind mit ihren geografischen Koordinaten angegeben, dazu
         alte und neue Ortsbezeichnungen. Mit den Farbfotografien möchte ich
         gerne die Stimmung der heutigen Plätze festhalten, sie sollen keine
         Übersicht über den jetzigen Zustand darstellen. Ihnen entgegen steht das
-        historische Bildmaterial, das den zeitlichen Wandel der jeweiligen Orte
-        zeigt. Die Dokumente sind möglichst so herausgesucht, dass sie uns etwas
-        über den Grund der Profanierung verraten. Im Kontrast dazu stehen die
-        ausgewählten Zitate.
+        historische Bildmaterial, das den zeitlichen Wandel zeigt. Die Dokumente
+        sind möglichst so herausgesucht, dass sie uns etwas über den Grund der
+        Profanierung verraten. Im Kontrast dazu stehen die ausgewählten Zitate.
+        So ergibt sich ein neuer Blick auf das alltägliche Stadtbild.
       </StyledSpan>
     </p>
   );
